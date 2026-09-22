6ed9708ca725e2ea4975b9ecb06db35c15fa8a50
NabilHardian <hardiannabil0412@gmail.com>
2026-09-22 09:27:49 +0700

    revisi

--- app/Http/Controllers/Admin/PasienController.php
+++ app/Http/Controllers/Admin/PasienController.php
@@ -31,16 +31,11 @@ class PasienController extends Controller
             'password' => 'required|string|min:6',
         ]);
 
-        $lastPasien = User::where('role', 'pasien')->orderBy('id', 'desc')->first();
-        $lastId = $lastPasien ? $lastPasien->id + 1 : 1;
-        $no_rm = date('Ym') . '-' . str_pad($lastId, 3, '0', STR_PAD_LEFT);
-
         User::create([
             'nama' => $request->nama,
             'alamat' => $request->alamat,
             'no_ktp' => $request->no_ktp,
             'no_hp' => $request->no_hp,
-            'no_rm' => $no_rm,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'role' => 'pasien',
